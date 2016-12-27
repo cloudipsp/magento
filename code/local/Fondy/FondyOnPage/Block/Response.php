b778216671748e7bacca9e8942fc8f2d1a991a8e
dmiroshnikov <dmmatrix222@gmail.com>
2016-12-27 13:27:29 +0200

    info to admin

--- code/local/Fondy/FondyOnPage/Block/Response.php
+++ code/local/Fondy/FondyOnPage/Block/Response.php
@@ -26,12 +26,12 @@ class Fondy_FondyOnPage_Block_Response extends Mage_Core_Block_Abstract
                 $order = Mage::getModel('sales/order');
                 $order->loadByIncrementId($orderId);
                 if ($fodny->getConfigData('after_pay_status') == Mage_Sales_Model_Order::STATE_PROCESSING){
-                    $order->setState($fodny->getConfigData('after_pay_status'), true, 'Gateway has authorized the payment.');
+                    $order->setState($fodny->getConfigData('after_pay_status'), true, 'Gateway has authorized the payment. ' . $_POST['order_id']);
                 }elseif($fodny->getConfigData('after_pay_status') == Mage_Sales_Model_Order::STATE_HOLDED){
-                    $order->setState($fodny->getConfigData('after_pay_status'), true, 'Gateway has authorized the payment.');
+                    $order->setState($fodny->getConfigData('after_pay_status'), true, 'Gateway has authorized the payment. ' . $_POST['order_id']);
                 }
                 else{
-                    $order->setState(Mage_Sales_Model_Order::STATE_PROCESSING, true, 'Gateway has authorized the payment.');
+                    $order->setState(Mage_Sales_Model_Order::STATE_PROCESSING, true, 'Gateway has authorized the payment. ' . $_POST['order_id']);
                 }
 
                 $order->sendNewOrderEmail();
@@ -45,7 +45,7 @@ class Fondy_FondyOnPage_Block_Response extends Mage_Core_Block_Abstract
                 Mage::app()->getFrontController()->getResponse()->setRedirect($url);
             } else {
                 // case all is valid but order is not approved
-                $url = Mage::getUrl('checkout/onepage/success', array('_secure' => true));
+                $url = Mage::getUrl('checkout/onepage/error', array('_secure' => true));
                 Mage::app()->getFrontController()->getResponse()->setRedirect($url);
             }
         } catch (Exception $e) {
