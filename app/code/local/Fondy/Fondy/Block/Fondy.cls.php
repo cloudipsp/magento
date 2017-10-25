224a19ef7f7dbccb65a9c59f2cd0c97e13219ce0
dmmatrix222 <dmmatrix222@gmail.com>
2017-10-25 17:14:11 +0300

    fix old class

--- app/code/local/Fondy/Fondy/Block/Fondy.cls.php
+++ app/code/local/Fondy/Fondy/Block/Fondy.cls.php
@@ -1,43 +1,13 @@
 <?php
 class FondyForm
 {
+    const RESPONCE_SUCCESS = 'success';
+    const RESPONCE_FAIL = 'failure';
+    const ORDER_SEPARATOR = '#';
+    const SIGNATURE_SEPARATOR = '|';
     const ORDER_APPROVED = 'approved';
     const ORDER_DECLINED = 'declined';
-
-    const ORDER_SEPARATOR = '#';
-
-    const SIGNATURE_SEPARATOR = '|';
-
-    const URL = "https://api.fondy.eu/api/checkout/redirect/";
-
-    protected static $responseFields = array('rrn',
-        'masked_card',
-        'sender_cell_phone',
-        'response_status',
-        'currency',
-        'fee',
-        'reversal_amount',
-        'settlement_amount',
-        'actual_amount',
-        'order_status',
-        'response_description',
-        'order_time',
-        'actual_currency',
-        'order_id',
-        'tran_type',
-        'eci',
-        'settlement_date',
-        'payment_system',
-        'approval_code',
-        'merchant_id',
-        'settlement_currency',
-        'payment_id',
-        'sender_account',
-        'card_bin',
-        'response_code',
-        'card_type',
-        'amount',
-        'sender_email');
+	const URL = "https://api.fondy.eu/api/checkout/redirect/";
 
     public static function getSignature($data, $password, $encoded = true)
     {
@@ -45,44 +15,41 @@ class FondyForm
             return $var !== '' && $var !== null;
         });
         ksort($data);
-
         $str = $password;
         foreach ($data as $k => $v) {
-            $str .= self::SIGNATURE_SEPARATOR . $v;
+            $str .= FondyForm::SIGNATURE_SEPARATOR . $v;
         }
-
         if ($encoded) {
             return sha1($str);
         } else {
             return $str;
         }
     }
-
-    public static function isPaymentValid($fondySettings, $response)
+    public static function isPaymentValid($oplataSettings, $response)
     {
-        if ($fondySettings['merchant_id'] != $response['merchant_id']) {
-            Mage::throwException('An error has occurred during payment. Merchant data is incorrect.');
-        }
 
-        if ($response['order_status'] == self::ORDER_DECLINED) {
+        if ($oplataSettings['merchant_id'] != $response['merchant_id']) {
+            return 'An error has occurred during payment. Merchant data is incorrect.';
+        }
+        if ($response['order_status'] == FondyForm::ORDER_DECLINED) {
             Mage::throwException('An error has occurred during payment. Order is declined.');
         }
-
-        $originalResponse = $response;
-        foreach ($response as $k => $v) {
-            if (!in_array($k, self::$responseFields)) {
-                unset($response[$k]);
-            }
+		if ($response['order_status'] != FondyForm::ORDER_APPROVED) {
+            Mage::throwException('An error has occurred during payment. Order is not approv.');
         }
-
-        if (self::getSignature($response, $fondySettings['secret_key']) != $originalResponse['signature']) {
+			$responseSignature = $response['signature'];
+		if (isset($response['response_signature_string'])){
+			unset($response['response_signature_string']);
+		}
+		if (isset($response['signature'])){
+			unset($response['signature']);
+		}
+		if (self::getSignature($response, $oplataSettings['secret_key']) != $responseSignature) {
             Mage::throwException('An error has occurred during payment. Signature is not valid.');
         }
+		return true;
 
-        if ($response['order_status'] != self::ORDER_APPROVED) {
-            return false;
-        }
 
-        return true;
-    }
+	}
 }
+
